dfe3a954275557d306926a90471c79636f460f7b
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-05-19 22:22:18 +0530

    added icons

---
+++ client/src/assets/icons/search.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Search = ({ color = 'black', size = 22,  className ='' }) => (
+    <svg
+        width={size}
+        height={size}
+        viewBox="0 0 22 22"
+        className={className}
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M17.5 17.5L21 21M20 10.5C20 5.25329 15.7467 1 10.5 1C5.25329 1 1 5.25329 1 10.5C1 15.7467 5.25329 20 10.5 20C15.7467 20 20 15.7467 20 10.5Z"
+            stroke={color}
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
+);
+
+export default Search;
