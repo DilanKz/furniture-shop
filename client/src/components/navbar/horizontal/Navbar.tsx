9e5c6c02c49ef8c686d24541ba33afbe8e161bea
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-05-23 17:28:30 +0530

    Update Navbar.tsx

--- client/src/components/navbar/horizontal/Navbar.tsx
+++ client/src/components/navbar/horizontal/Navbar.tsx
@@ -1,4 +1,4 @@
-import {Fragment} from "react";
+import React from "react";
 
 import {Badge, Col, List, ListGroupItem, Row} from "reactstrap";
 
@@ -6,26 +6,27 @@ import logo from '../../../assets/img/Essence.png'
 import Search from "../../../assets/icons/search";
 import Bag from "../../../assets/icons/bag";
 import User from "../../../assets/icons/user";
+
 const Navbar = () => {
     return (
-        <Fragment>
-            <Col xs={12} className={'position-fixed top-0 start-0 bg-theme-neutral-one'}>
+        <div className={'vw-100 overflow-hidden'}>
+            <Col xs={12} className={'position-fixed top-0 start-0 bg-theme-neutral-one vw-100'}>
                 <Row className={'m-0 justify-content-between'}>
                     <Col lg={2} xs={4} className={'my-3'}>
                         <img src={logo} alt="" className={'w-50 mt-2 ms-4'}/>
                     </Col>
-                    <Col lg={4} className={'d-lg-flex d-none items-center'}>
+                    <Col lg={5} className={'d-lg-flex d-none items-center'}>
                         <List className={'d-flex align-items-center mb-0'}>
-                            <ListGroupItem className={'text-theme-main cursor-pointer'}>
+                            <ListGroupItem className={'text-theme-main cursor-pointer text-[16px]'}>
                                 Home
                             </ListGroupItem>
-                            <ListGroupItem className={'ms-5 text-theme-neutral-four cursor-pointer'}>
+                            <ListGroupItem className={'ms-5 text-theme-neutral-four cursor-pointer text-[16px]'}>
                                 Shop
                             </ListGroupItem>
-                            <ListGroupItem className={'ms-5 text-theme-neutral-four cursor-pointer'}>
+                            <ListGroupItem className={'ms-5 text-theme-neutral-four cursor-pointer text-[16px]'}>
                                 Product
                             </ListGroupItem>
-                            <ListGroupItem className={'ms-5 text-theme-neutral-four cursor-pointer'}>
+                            <ListGroupItem className={'ms-5 text-theme-neutral-four cursor-pointer text-[16px]'}>
                                 Contact Us
                             </ListGroupItem>
                         </List>
@@ -44,7 +45,7 @@ const Navbar = () => {
                     </Col>
                 </Row>
             </Col>
-        </Fragment>
+        </div>
     );
 };
 
